a8dee2108ca1d95226a69c541e44976af782e1a2
ShatterdPotato <saahmed5526@gmail.com>
2024-10-25 13:47:31 -0400

    More lines of code

--- src/SecondRunner.java
+++ src/SecondRunner.java
@@ -2,5 +2,6 @@ public class SecondRunner {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("hello");
+        System.out.println("last one i promise");
     }
 }
