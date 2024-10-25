3c7cc6be7f1bff085a0fd3d73cdaa552b7bb39da
ShatterdPotato <saahmed5526@gmail.com>
2024-10-25 13:11:51 -0400

    added functionality

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Runner {
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        Scanner scan = new Scanner(System.in);
+        boolean isFriday;
+
+        System.out.print("What day is it today?: ");
+        String answer = scan.nextLine();
+
+        if (answer.toLowerCase().equals("friday")) {
+            isFriday = true;
+        }   else {
+            isFriday = false;
+        }
+
+        System.out.println(isFriday);
     }
 }
